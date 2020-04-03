0228fa9a7659049dd5522edaffeae057b9dc4a66
robrtruns <robrtjaxon@gmail.com>
2020-04-03 10:28:58 -0500

    Added footer and homepage route

---
+++ petplayground/src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+function footer() {
+  return (
+    <footer className="footer">
+      <span>PetLife 2019</span>
+    </footer>
+  );
+}
+export default footer;
