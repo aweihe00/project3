99b93238e302fe9c746e8f816a730f2fe2f11587
robrtruns <robrtjaxon@gmail.com>
2020-04-03 11:08:44 -0500

    Working on file upload

---
+++ petplayground/client/src/upload/fileUpload.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Images from "./Images";
+import { API } from "../utils/API";
+export default class FileUpload extends Component {
+    state = {
+        uploading: false,
+        images: []
+    }
+    
+}
+
+require("dotenv").config();
+const cors = require("cors");
+const cloudinary = require("cloudinary");
+const formData = require("express-form-data");
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
+})
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+app.post("/upload", upload);
+Collapse
+
+
+
+
+
