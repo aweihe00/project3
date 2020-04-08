bde7fb1a73e200b70fd0e0a386feeeb634fb5dd1
robrtruns <robrtjaxon@gmail.com>
2020-04-07 20:33:48 -0500

    Renaming files

--- petplayground/client/src/pages/petInfo.js
+++ petplayground/client/src/pages/petInfo.js
@@ -1,56 +1,53 @@
 import React from "react";
-import "./petInfo.css";
 import API from "../utils/API2";
 import axios from "axios";
-
+import PetInfoCard from "../components/petInfoCard/petInfoCard";
 class PetInfo extends React.Component {
-
   state = {
     petName: "",
-    nicknames: "",
+    nickname: "",
     breed: "",
     birthday: "",
-    allergies: "", 
-    temperament: "", 
+    allergies: "",
+    temperament: "",
     diet: "",
     directions: "",
-    id: "5d8c2b38e773341971f43e8c"
-
-};
-
-componentDidMount () {
-  axios.get("/api/getPets/" + this.state.id).then( data => {
-    console.log(data)
-    this.setState({ 
-      petName: data.data.name,
-      breed: data.data.breed,
-      diet: data.data.diet,
-      birthday: data.data.birthday.slice(0, -14),
-      temperament: data.data.temperament,
-      nicknames: data.data.nicknames })
-  })
-}
-
+    id: " "
+  };
+  componentDidMount() {
+    axios.get("/api/getPets/" + this.state.id).then(data => {
+      console.log(data);
+      this.setState({
+        // petName: data.data.name,
+        // breed: data.data.breed,
+        // diet: data.data.diet,
+        // birthday: data.data.birthday.slice(0, -14),
+        // temperament: data.data.temperament,
+        // nickname: data.data.nicknames
+      });
+    });
+  }
   render() {
-  return (
-    <div className="petInfoCont">
-      <div className="container">
-        <h1>Pet Info</h1>
-      </div>
-      <div className="jumbotron petInfoJumbo">
-        <div className="col text-right petInfoImage"></div>
-        <div className="col text-start">
-          <p>Name: {this.state.petName}</p>
-          <p>Nicknames: {this.state.nicknames}</p>
-          <p>Breed: {this.state.breed}</p>
-          <p>Birthday: {this.state.birthday}</p>
-          <p>Temperament: {this.state.temperament}</p>
-          <p>Diet: {this.state.diet}</p>
+    return (
+      <div className="petInfoCont">
+        <div className="container">
+          <h1>Pet Info</h1>
         </div>
+        <PetInfoCard
+          img="https://via.placeholder.com/150"
+          name={this.state.petName}
+          breed={this.state.breed}
+          nickname={this.state.petName}
+          birthday={this.state.birthday}
+          temperament={this.state.temperament}
+          diet={this.state.diet}
+        />
       </div>
-    </div>
-  );
+    );
+  }
 }
-}
-
 export default PetInfo;
+
+
+
+
