05d80190896083670292a4b025d2d9859cdba22c
robrtruns <robrtjaxon@gmail.com>
2020-04-07 19:38:33 -0500

    Added nicknames for pets, still need to fix bdays yo

--- petplayground/controllers/petController.js
+++ petplayground/controllers/petController.js
@@ -17,7 +17,7 @@ module.exports = {
             .catch(err => res.status(422).json(err))
     },
     create: function (req, res) {
-        console.log("hit")
+        console.log(reg.body)
         db.Pet
             .create(req.body)
             .then(dbModel => {
