06bcb6e498c887373e4f901eafc382476dd6b0c0
robrtruns <robrtjaxon@gmail.com>
2020-04-09 20:06:10 -0500

    Working on rendering visits to page

--- petplayground/client/src/components/PastVisits/index.js
+++ petplayground/client/src/components/PastVisits/index.js
@@ -1,52 +1,80 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import UserContext from "../../context/UserContext";
 class PastVisits extends Component {
+    static contextType = UserContext
     state = {
+        mounted: false,
+        refreshed: false,
         results: [
-            {
-                date: "1",
-                doctorsName: "First doctorsName",
-                hospital: "pet Hospital 1",
-            },
-            {
-                date: "2",
-                doctorsName: "Second doctorsName",
-                hospital: "pet Hospital 2",
-            }
+            // {
+            //     date: "1",
+            //     doctorsName: "First doctorsName",
+            //     hospital: "pet Hospital 1",
+            // },
+            // {
+            //     date: "2",
+            //     doctorsName: "Second doctorsName",
+            //     hospital: "pet Hospital 2",
+            // }
         ]
     }
-
-render() {
-    return (
-        < >
-            <h2>Past Visits</h2>
-            <table className="table table-dark">
-                <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Doctors name</th>
-                        <th scope="col">Hospital</th>
-                        <th scope="col">Details</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    {this.state.results.map(result => (
+    // componentDidUpdate() {
+    //     if (this.state.mounted == false) {
+    //         if (this.state.refreshed == false) {
+    //             axios.get(`/api/user/${this.context.user.id}/visits`)
+    //                 .then(function (res) {
+    //                     console.log(res.data);
+    //                     this.setState({
+    //                         results: res.data,
+    //                         refreshed: true
+    //                     });
+    //                 });
+    //         } else {
+    //             this.setState({
+    //                 mounted: true
+    //             })
+    //         }
+    //     }
+    // }
+    componentDidMount() {
+        if (!this.context.user) return
+        axios.get(`/api/user/${this.context.user.id}/visits`)
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    results: res.data,
+                    mounted: true
+                })
+            })
+    }
+    render() {
+        return (
+            < >
+                <h2>Past Visits</h2>
+                <table className="table table-dark">
+                    <thead>
                         <tr>
-                            <td>{result.date}</td>
-                            <td>{result.doctorsName}</td>
-                            <td>{result.hospital}</td>
-                            <td> <Link to="/visits/viewDetail">See Details</Link></td>
+                            <th scope="col">Date</th>
+                            <th scope="col">Doctors name</th>
+                            <th scope="col">Hospital</th>
+                            <th scope="col">Details</th>
                         </tr>
-                    ))}
-                
+                    </thead>
+                    <tbody>
+                        {this.state.results.map(result => (
+                            <tr>
+                                <td>{result.date}</td>
+                                <td>{result.doctorsName}</td>
+                                <td>{result.hospital}</td>
+                                <td> <Link to="/visits/viewDetail" >See Details</Link></td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </>
         );
     }
 }
-
 export default PastVisits;
