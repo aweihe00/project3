012bcc74e94b1147a28d4214c14ea4b5a2dc5db4
Andrew Weihe <andrewy03@gmail.com>
2020-04-09 20:26:53 -0500

    resloving missing reference in routes

--- petplayground/server.js
+++ petplayground/server.js
@@ -2,43 +2,47 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-// for file upload component
+
 const cors = require("cors");
-const cloudinary = require("cloudinary");
 const formData = require("express-form-data");
 const { CLIENT_ORIGIN } = require("./config");
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-})
+
 app.use(cors({
   origin: CLIENT_ORIGIN
 }));
+
 app.use(formData.parse());
-// Middleware for app
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/build")));
 }
+
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/dbPet";
+
 mongoose
   .connect(MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
-// require("./routes/html-routes.js")(app);
+
 require("./routes/api-routes.js")(app);
+
 if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
+
 app.listen(PORT, function () {
   console.log("App listening on Port: " + PORT);
 });
